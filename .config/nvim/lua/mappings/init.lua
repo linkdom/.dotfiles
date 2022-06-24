248b09aa9ae7ee3403bf1afeb5fbe1f24df01581
Dominik Link <Vorbild94@gmail.com>
2022-06-24 12:56:05 +0200

    remapped escape to home row

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -3,9 +3,12 @@ local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
 
+map('i', '<leader>j', '<esc>', opts)
+
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
 map('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
+
 map('n', 'gcap', '@c', opts)
 map('n', 'guap', '@u', opts)
 map('n', 'gcl', '@l', opts)
