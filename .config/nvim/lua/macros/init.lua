754cb6bdce9c29b52ac1156c6b943a97463464f7
Dominik Link <Vorbild94@gmail.com>
2022-06-29 20:23:59 +0200

    fixed closing of all buffers

--- .config/nvim/lua/macros/init.lua
+++ .config/nvim/lua/macros/init.lua
@@ -2,4 +2,4 @@ vim.fn.setreg('c', '{}kI//xxj')      -- comment block code
 vim.fn.setreg('u', '{}kx.j')        -- uncomment block code
 vim.fn.setreg('l', '0wi//')        -- comment line
 vim.fn.setreg('z', '0wxx')        -- uncomment line
-vim.fn.setreg('d', ':%bd|e#)        -- close all buffers except current one
+vim.fn.setreg('d', ':%bd|e#\')        -- close all buffers except current one
